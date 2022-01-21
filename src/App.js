4e0774a7893f8d09b3306e278aa43529d5fe55ca
Wolftail8192 <4420299@gmail.com>
2022-01-21 14:10:51 +0200

    base1.2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import axios from "axios"
 
 import {Routes, Route} from "react-router-dom";
 
-
+import css from "./App.module.css"
+import Users from "./pages/Users/Users"
+import Posts from "./pages/Posts/Posts"
+import NotFound from "./pages/NotFound/NotFound"
 
 const App = () => {
     return (
         <div>
-          
+          <div className={css.header}>
+              <a href="/users"> Users</a>
+              <a href="/posts"> Posts</a>
+
+          </div>
+            <Routes>
+                <Route path={'/users'} element={<Users/>}/>
+                <Route path={'/posts'} element={<Posts/>}/>
+                <Route path={'*'} element={<NotFound/>}/>
+
+            </Routes>
         </div>
     );
 };
