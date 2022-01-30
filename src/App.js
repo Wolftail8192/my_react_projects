6af65a3d9b919c9880c66f759e958126055adfb1
Wolftail8192 <4420299@gmail.com>
2022-01-30 12:13:32 +0200

    1.0

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 
-const App = () => {
+const reducer = (state, action) => {
+    switch (action.type){
+        case 'inc':return {...state,count1: state.count1+1}
+
+        case 'dec':return {...state,count1: state.count1-1}
+
+        case 'reset':return {...state,count1: 0}
+    }
+
+  return state
+}
+
+function App  () {
+    const [state, dispatch] = useReducer(reducer, {count1:0})
     return (
         <div>
-          
+          <div>{state.count1}</div>
+            <button onClick={()=>dispatch({type:'inc'})}>INC</button>
+            <button onClick={()=>dispatch({type:'dec'})}>DEC</button>
+            <button onClick={()=>dispatch({type:'reset'})}>RESET</button>
         </div>
     );
 };
