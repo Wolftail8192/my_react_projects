cde00077c584b68f1054a4afff6f7d5a9b966401
Wolftail8192 <4420299@gmail.com>
2022-02-03 11:46:24 +0200

    mirror 5.1.1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import Form from "./components/Form/Form";
 import Todos from "./components/Todos/Todos";
+import css from "./App.module.css";
+
+
 
 const App = () => {
     return (
         <div>
-            <Form/>
-            <Todos/>
+            <div className={css.form}>
+                <Form/>
+            </div>
+            <div className={css.Todos}>
+                <Todos/>
+            </div>
         </div>
     );
 };
