c8290399136089250aeb90ed2e54eae4a22f6f65
Wolftail8192 <4420299@gmail.com>
2022-02-11 06:43:58 +0200

    mini_pro_react 3.2

--- src/components/Movies/movies.tsx
+++ src/components/Movies/movies.tsx
@@ -1,4 +1,11 @@
+import {getMovies} from '../../services/api.service'
+import {useEffect} from "react";
+
 export function Movies(){
+
+    useEffect(()=>{
+        getMovies().then(value => console.log(value.data))
+    },[])
     return(
         <div>
             Movies
--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL:''
+let axiosInstance = axios.create({
+    baseURL:'https://api.themoviedb.org/3/discover/movie?api_key=4a7c600cc60d263ab60c8d32ea65cb23'
 })
+
+export const getMovies=()=>{return axiosInstance.get('/movie/{movie_id}')}
+
+export default axiosInstance
+
